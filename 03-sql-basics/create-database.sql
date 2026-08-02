c12a8724161e227bc67820db2274bdaeaa9587f3
MohammadReza <mohammadrezajf.2003@gmail.com>
2026-08-03 03:19:51 +0330

    feat(chapter-03): implement SQL basics and CRUD operations

---
+++ 03-sql-basics/create-database.sql
@@ -0,0 +1,20 @@
+/*
+==================================================
+Chapter 03 - SQL Basics
+
+File:
+01-create-database.sql
+
+Project:
+BookStore Management System
+
+Description:
+Creating the first database.
+==================================================
+*/
+
+
+-- Create database
+
+CREATE DATABASE bookstore;
+
---
+++ 03-sql-basics/insert-users.sql
@@ -0,0 +1,49 @@
+/*
+===========================================
+Chapter 03
+File: 03-insert-users.sql
+Description: Insert data into users table
+===========================================
+*/
+
+INSERT INTO users (
+    first_name,
+    last_name,
+    email,
+    phone
+)
+VALUES
+(
+    'Ali',
+    'Ahmadi',
+    'ali@example.com',
+    '09121234567'
+);
+
+INSERT INTO users (
+    first_name,
+    last_name,
+    email,
+    phone
+)
+VALUES
+(
+    'Sara',
+    'Mohammadi',
+    'sara@example.com',
+    '09123334455'
+);
+
+INSERT INTO users (
+    first_name,
+    last_name,
+    email,
+    phone
+)
+VALUES
+(
+    'Reza',
+    'Karimi',
+    'reza@example.com',
+    '09125556677'
+);
